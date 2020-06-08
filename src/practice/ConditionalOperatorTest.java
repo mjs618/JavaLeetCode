e6f341183ea148fe5e20d9fa899e78e7ef5175f0
maojinsong <1024mjs@gmail.com>
2020-06-08 12:17:25 +0800

    Practice conditional operators

---
+++ src/practice/ConditionalOperatorTest.java
@@ -0,0 +1,16 @@
+package practice;
+
+/**
+ * @author mjs20
+ */
+public class ConditionalOperatorTest {
+
+    public static void main(String[] args) {
+        int a, b = 0;
+        a = 10;
+        b = (a == 10) ? 20 :30;
+
+        System.out.println(b);
+
+    }
+}
